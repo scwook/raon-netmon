ed88014272c25199e4681d70fa5dde6c085d8e14
scwook <scwook@gmail.com>
2023-07-28 14:56:09 +0900

    right line showup anmiation test

--- lineAnimation.js
+++ lineAnimation.js
@@ -1,113 +1,309 @@
-var lineGradientInfo = {
-    'control' : {
-        'id' : 'control-line',
-        'color' : '#0C68AC',
-        'x1' : '115',
-        'y1' : '249',
-        'x2' : '372',
-        'y2' : '249'
+var svgInfo = {
+    'control': {
+        'line': {
+            'id': 'control-line',
+            'color': '#0C68AC',
+            'x1': '115',
+            'y1': '249',
+            'x2': '372',
+            'y2': '249'
+        },
+        'fill': {
+            'id': 'control-fill'
+        }
     },
-    'ctrluser' : {
-        'id' : 'ctrluser-line',
-        'color' : '#7D4E22',
-        'x1' : '70',
-        'y1' : '249',
-        'x2' : '272',
-        'y2' : '249'
+    'ctrluser': {
+        'line': {
+            'id': 'ctrluser-line',
+            'color': '#7D4E22',
+            'x1': '70',
+            'y1': '249',
+            'x2': '372',
+            'y2': '250'
+        },
+        'fill': {
+            'id': 'ctrluser-fill'
+        }
     },
-    'archive1' : {
-        'id' : 'archive1-line',
-        'color' : '#88157B',
-        'x1' : '40',
-        'y1' : '249',
-        'x2' : '272',
-        'y2' : '249'
+    'archive1': {
+        'line': {
+            'id': 'archive1-line',
+            'color': '#88157B',
+            'x1': '40',
+            'y1': '249',
+            'x2': '372',
+            'y2': '250'
+        },
+        'fill': {
+            'id': 'archive1-fill'
+        }
     },
-    'archive2' : {
-        'id' : 'archive2-line',
-        'color' : '#88157B',
-        'x1' : '40',
-        'y1' : '249',
-        'x2' : '272',
-        'y2' : '249'
+    'archive2': {
+        'line': {
+            'id': 'archive2-line',
+            'color': '#88157B',
+            'x1': '40',
+            'y1': '249',
+            'x2': '372',
+            'y2': '250'
+        },
+        'fill': {
+            'id': 'archive2-fill'
+        }
     },
-    'scl3Gateway1' : {
-        'id' : 'scl3-gateway1-line',
-        'color' : '#88157B',
-        'x1' : '15',
-        'y1' : '249',
-        'x2' : '272',
-        'y2' : '249'
+    'scl3Gateway1': {
+        'line': {
+            'id': 'scl3-gateway1-line',
+            'color': '#88157B',
+            'x1': '15',
+            'y1': '249',
+            'x2': '372',
+            'y2': '250'
+        },
+        'fill': {
+            'id': 'scl3-gateway1-fill'
+        }
     },
-    'scl3Gateway2' : {
-        'id' : 'scl3-gateway2-line',
-        'color' : '#88157B',
-        'x1' : '10',
-        'y1' : '249',
-        'x2' : '272',
-        'y2' : '249'
+    'scl3Gateway2': {
+        'line': {
+            'id': 'scl3-gateway2-line',
+            'color': '#88157B',
+            'x1': '10',
+            'y1': '249',
+            'x2': '372',
+            'y2': '250'
+        },
+        'fill': {
+            'id': 'scl3-gateway2-fill'
+        }
     },
-    'displayWall' : {
-        'id' : 'display-wall-line',
-        'color' : '#2CA339',
-        'x1' : '50',
-        'y1' : '249',
-        'x2' : '272',
-        'y2' : '249'
+    'displayWall': {
+        'line': {
+            'id': 'display-wall-line',
+            'color': '#2CA339',
+            'x1': '50',
+            'y1': '249',
+            'x2': '372',
+            'y2': '250'
+        },
+        'fill': {
+            'id': 'display-wall-fill'
+        }
     },
-    'operator' : {
-        'id' : 'operator-line',
-        'color' : '#F0E62A',
-        'x1' : '115',
-        'y1' : '249',
-        'x2' : '372',
-        'y2' : '249'
+    'operator': {
+        'line': {
+            'id': 'operator-line',
+            'color': '#F0E62A',
+            'x1': '115',
+            'y1': '249',
+            'x2': '372',
+            'y2': '250'
+        },
+        'fill': {
+            'id': 'operator-fill'
+        }
+    },
+    'ccsi1': {
+        'line': {
+            'id': 'ccsi1-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '189',
+            'x2': '116',
+            'y2': '189'
+        },
+        'fill': {
+            'id': 'ccsi1-fill'
+        }
+    },
+    'ccs02': {
+        'line': {
+            'id': 'ccs02-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '210',
+            'x2': '187',
+            'y2': '210'
+        },
+        'fill': {
+            'id': 'ccs02-fill'
+        }
+    },
+    'ccs05': {
+        'line': {
+            'id': 'ccs05-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '237',
+            'x2': '248',
+            'y2': '237'
+        },
+        'fill': {
+            'id': 'ccs05-fill'
+        }
+    },
+    'ccs09': {
+        'line': {
+            'id': 'ccs09-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '270',
+            'x2': '295',
+            'y2': '270'
+        },
+        'fill': {
+            'id': 'ccs09-fill'
+        }
+    },
+    'ccs12': {
+        'line': {
+            'id': 'ccs12-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '307',
+            'x2': '328',
+            'y2': '307'
+        },
+        'fill': {
+            'id': 'ccs12-fill'
+        }
+    },
+    'ccs14': {
+        'line': {
+            'id': 'ccs14-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '347',
+            'x2': '343',
+            'y2': '347'
+        },
+        'fill': {
+            'id': 'ccs14-fill'
+        }
+    },
+    'ccs16': {
+        'line': {
+            'id': 'ccs16-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '387',
+            'x2': '343',
+            'y2': '387'
+        },
+        'fill': {
+            'id': 'ccs16-fill'
+        }
+    },
+    'ccs21': {
+        'line': {
+            'id': 'ccs21-line',
+            'color': '#88157B',
+            'x1': '0',
+            'y1': '427',
+            'x2': '328',
+            'y2': '427'
+        },
+        'fill': {
+            'id': 'ccs21-fill'
+        }
+    },
+    'ccs24': {
+        'line': {
+            'id': 'ccs24-line',
+            'color': '#E99413',
+            'x1': '0',
+            'y1': '465',
+            'x2': '295',
+            'y2': '465'
+        },
+        'fill': {
+            'id': 'ccs24-fill'
+        }
+    },
+    'ccs29': {
+        'line': {
+            'id': 'ccs29-line',
+            'color': '#E99413',
+            'x1': '0',
+            'y1': '497',
+            'x2': '247',
+            'y2': '497'
+        },
+        'fill': {
+            'id': 'ccs29-fill'
+        }
+    },
+    'ccs34': {
+        'line': {
+            'id': 'ccs34-line',
+            'color': '#E99413',
+            'x1': '0',
+            'y1': '525',
+            'x2': '187',
+            'y2': '525'
+        },
+        'fill': {
+            'id': 'ccs34-fill'
+        }
+    },
+    'ccs40': {
+        'line': {
+            'id': 'ccs40-line',
+            'color': '#E99413',
+            'x1': '0',
+            'y1': '525',
+            'x2': '116',
+            'y2': '525'
+        },
+        'fill': {
+            'id': 'ccs40-fill'
+        }
     }
-    
 }
 
+function leftLineShowUp(line, showUpDelay) {
+    let lineInfo = line;
+    let delay = showUpDelay;
+    let duration = 2;
 
-
-function lineShowUp(lineInfo) {
-    let line = lineInfo;
-    let gradientID = line.id + '-gradient';
+    let gradientID = lineInfo.line.id + '-gradient';
 
     let linearGr = document.createElementNS("http://www.w3.org/2000/svg", "linearGradient");
     linearGr.setAttribute('id', gradientID);
     linearGr.setAttribute('gradientUnits', 'userSpaceOnUse');
-    linearGr.setAttribute('x1', line.x1);
-    linearGr.setAttribute('y1', line.y1);
-    linearGr.setAttribute('x2', line.x2);
-    linearGr.setAttribute('y2', line.y2);
+    linearGr.setAttribute('x1', lineInfo.line.x1);
+    linearGr.setAttribute('y1', lineInfo.line.y1);
+    linearGr.setAttribute('x2', lineInfo.line.x2);
+    linearGr.setAttribute('y2', lineInfo.line.y2);
 
     let stop1 = document.createElementNS("http://www.w3.org/2000/svg", 'stop');
     stop1.setAttribute('offset', '1');
-    stop1.style.stopColor = "#FFFFFF";
+    stop1.style.stopColor = lineInfo.line.color;
     stop1.style.stopOpacity = "0";
 
     let stop2 = document.createElementNS("http://www.w3.org/2000/svg", 'stop');
     stop2.setAttribute('offset', '1');
-    stop2.style.stopColor = line.color;
+    stop2.style.stopColor = lineInfo.line.color;
 
     let ani1 = document.createElementNS("http://www.w3.org/2000/svg", 'animate');
     ani1.setAttribute("attributeName", "offset");
-    ani1.setAttribute('begin', '0s');
-    ani1.setAttribute('dur', '0.5s');
+    ani1.setAttribute('begin', delay);
+    ani1.setAttribute('dur', duration);
     ani1.setAttribute('fill', 'freeze');
     ani1.setAttribute('from', '1');
     ani1.setAttribute('to', '0');
-    ani1.setAttribute('keySplines', "0.1 0.2 0.4 1");
+    ani1.setAttribute('keySplines', "0.6 0 0.4 1");
     ani1.setAttribute('keyTimes', "0;1");
     ani1.setAttribute('calcMode', "spline");
 
     let ani2 = document.createElementNS("http://www.w3.org/2000/svg", 'animate');
     ani2.setAttribute("attributeName", "offset");
-    ani2.setAttribute('begin', '0.2s');
-    ani2.setAttribute('dur', '0.5s');
+    ani2.setAttribute('begin', delay + 0.2);
+    ani2.setAttribute('dur', duration);
     ani2.setAttribute('fill', 'freeze');
     ani2.setAttribute('from', '1');
     ani2.setAttribute('to', '0');
-    ani2.setAttribute('keySplines', "0.1 0.2 0.4 1");
+    ani2.setAttribute('keySplines', "0.6 0 0.4 1");
     ani2.setAttribute('keyTimes', "0;1");
     ani2.setAttribute('calcMode', "spline");
 
@@ -120,7 +316,65 @@ function lineShowUp(lineInfo) {
     let lineGroup = document.getElementById('left-line-group');
     lineGroup.appendChild(linearGr);
 
+    let id = document.getElementById(lineInfo.line.id);
+    id.style.stroke = `url(#${gradientID}`;
+}
 
-    let id = document.getElementById(line.id);
+function rightLineShowUp(line, showUpDelay) {
+    let lineInfo = line;
+    let delay = showUpDelay;
+    let duration = 2;
+
+    let gradientID = lineInfo.line.id + '-gradient';
+
+    let linearGr = document.createElementNS("http://www.w3.org/2000/svg", "linearGradient");
+    linearGr.setAttribute('id', gradientID);
+    linearGr.setAttribute('gradientUnits', 'userSpaceOnUse');
+    linearGr.setAttribute('x1', lineInfo.line.x1);
+    linearGr.setAttribute('y1', lineInfo.line.y1);
+    linearGr.setAttribute('x2', lineInfo.line.x2);
+    linearGr.setAttribute('y2', lineInfo.line.y2);
+
+    let stop1 = document.createElementNS("http://www.w3.org/2000/svg", 'stop');
+    stop1.setAttribute('offset', '0');
+    stop1.style.stopColor = lineInfo.line.color;
+
+    let stop2 = document.createElementNS("http://www.w3.org/2000/svg", 'stop');
+    stop2.setAttribute('offset', '0');
+    stop2.style.stopColor = lineInfo.line.color;
+    stop2.style.stopOpacity = "0";
+
+    let ani1 = document.createElementNS("http://www.w3.org/2000/svg", 'animate');
+    ani1.setAttribute("attributeName", "offset");
+    ani1.setAttribute('begin',  delay + 0.2);
+    ani1.setAttribute('dur', duration + 0.4);
+    ani1.setAttribute('fill', 'freeze');
+    ani1.setAttribute('from', '0');
+    ani1.setAttribute('to', '1');
+    ani1.setAttribute('keySplines', "0.6 0 0.4 1");
+    ani1.setAttribute('keyTimes', "0;1");
+    ani1.setAttribute('calcMode', "spline");
+
+    let ani2 = document.createElementNS("http://www.w3.org/2000/svg", 'animate');
+    ani2.setAttribute("attributeName", "offset");
+    ani2.setAttribute('begin', delay);
+    ani2.setAttribute('dur', duration);
+    ani2.setAttribute('fill', 'freeze');
+    ani2.setAttribute('from', '0');
+    ani2.setAttribute('to', '1');
+    ani2.setAttribute('keySplines', "0.6 0 0.4 1");
+    ani2.setAttribute('keyTimes', "0;1");
+    ani2.setAttribute('calcMode', "spline");
+
+    stop1.appendChild(ani1);
+    stop2.appendChild(ani2);
+
+    linearGr.appendChild(stop1);
+    linearGr.appendChild(stop2);
+
+    let lineGroup = document.getElementById('right-line-group');
+    lineGroup.appendChild(linearGr);
+
+    let id = document.getElementById(lineInfo.line.id);
     id.style.stroke = `url(#${gradientID}`;
 }
