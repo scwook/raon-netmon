17420eda8f7caa1554e8290f769c3532da47c3f5
scwook <scwook@ibs.re.kr>
2024-06-27 15:23:44 +0900

    MR

---
+++ experiment/config.js
@@ -0,0 +1,15 @@
+// Server Address
+var SERVER_ADDR = ""
+
+// InfluxDB Address
+var INFLUXDB_ADDR = "http://192.168.131.177:8086"
+
+// InfluxDB Authorization Token
+var INFLUXDB_TOKEN = "ttqdyr0xBXyR2QX5bcsMyEAV3B3xoFtdNSZw6795t-wsK-fFbIiBvLYjCnfk50zbXTwhiaFaaf3IFZBOiVqJmg=="; // RAON
+// var INFLUXDB_TOKEN = "6UyxcltMVociLrcCamGD1XzbfoQ5OSV4xjIU2waBfLM7fkfj6kRN0lNWIfgGl7PhXU5TfY33RvjgS0LaCWdfog=="; //HOME
+
+// InfluxDB Organization
+var ORGANIZATION = "KoBRA";
+
+// InfluxDB Bucket
+var BUCKET = "SWITCH";
