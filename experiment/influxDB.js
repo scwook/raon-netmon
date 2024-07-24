58dabf025e1192b07daf3cf7e1d08ebc46255401
scwook <scwook@ibs.re.kr>
2024-07-24 11:16:33 +0900

    kobra done, ndps working

--- experiment/influxDB.js
+++ experiment/influxDB.js
@@ -79,6 +79,34 @@ r["_field"] == "input-Gi1-0-24" or \
 r["_field"] == "output-Te1-0-01") \
 |> limit(n:2, offset: 0)`
 
+var ndpsTOFMainQuery = `from(bucket: "${bucket}") \
+|> range(start: -60s) \
+|> filter(fn: (r) => r["_measurement"] == "snmp") \
+|> filter(fn: (r) => r["agent_host"] == "192.168.135.15") \
+|> filter(fn: (r) => \
+r["_field"] == "output-Gi1-25") \
+|> limit(n:2, offset: 0)`
+
+var ndpsPSRoomMainQuery = `from(bucket: "${bucket}") \
+|> range(start: -60s) \
+|> filter(fn: (r) => r["_measurement"] == "snmp") \
+|> filter(fn: (r) => r["agent_host"] == "192.168.135.230") \
+|> filter(fn: (r) => \
+r["_field"] == "input-eth1-01-13" or \
+r["_field"] == "output-eth1-01-29") \
+|> limit(n:2, offset: 0)`
+
+var ndpsPSRoomQuery = `from(bucket: "${bucket}") \
+|> range(start: -60s) \
+|> filter(fn: (r) => r["_measurement"] == "snmp") \
+|> filter(fn: (r) => r["agent_host"] == "192.168.135.231") \
+|> filter(fn: (r) => \
+r["_field"] == "input-Gi1-02" or \
+r["_field"] == "input-Gi1-08" or \
+r["_field"] == "output-Te1-49") \
+|> limit(n:2, offset: 0)`
+
+
 var queryData = {
     "kobraE1TPS":
     {
@@ -158,6 +186,54 @@ var queryData = {
         "now": isoTime,
         "params": {},
         "query": kobraCountingRoomQuery,
+        "type": "flux"
+    },
+    "ndpsTOFMain":
+    {
+        "dialect": {
+            "annotations": [
+                "group"
+            ],
+            "commentPrefix": "#",
+            "dateTimeFormat": "RFC3339",
+            "delimiter": ",",
+            "header": true
+        },
+        "now": isoTime,
+        "params": {},
+        "query": ndpsTOFMainQuery,
+        "type": "flux"
+    },
+    "ndpsPSRoomMain":
+    {
+        "dialect": {
+            "annotations": [
+                "group"
+            ],
+            "commentPrefix": "#",
+            "dateTimeFormat": "RFC3339",
+            "delimiter": ",",
+            "header": true
+        },
+        "now": isoTime,
+        "params": {},
+        "query": ndpsPSRoomMainQuery,
+        "type": "flux"
+    },
+    "ndpsPSRoom":
+    {
+        "dialect": {
+            "annotations": [
+                "group"
+            ],
+            "commentPrefix": "#",
+            "dateTimeFormat": "RFC3339",
+            "delimiter": ",",
+            "header": true
+        },
+        "now": isoTime,
+        "params": {},
+        "query": ndpsPSRoomQuery,
         "type": "flux"
     }
 };
@@ -495,131 +571,29 @@ function monitoringKoBRACountingRoomSwitch(interval) {
     queryData.kobraCountingRoom.now = isoTime;
 }
 
-function monitoringKoBRAe1TPSSwitch(interval) {
+function monitoringNDPSTOFMainSwitch(interval) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
 
             let data = this.responseText.split(",");
 
-            let index_input1_1 = data.indexOf("input-Gi1-0-01");
-            let index_input1_2 = data.indexOf("input-Gi1-0-01", index_input1_1 + 1);
-
-            let data_input1_1 = data[index_input1_1 - 1];
-            let data_input1_2 = data[index_input1_2 - 1];
-
-            let index_input2_1 = data.indexOf("input-Gi1-0-02");
-            let index_input2_2 = data.indexOf("input-Gi1-0-02", index_input2_1 + 1);
-
-            let data_input2_1 = data[index_input2_1 - 1];
-            let data_input2_2 = data[index_input2_2 - 1];
-
-            let index_input3_1 = data.indexOf("input-Gi1-0-03");
-            let index_input3_2 = data.indexOf("input-Gi1-0-03", index_input3_1 + 1);
-
-            let data_input3_1 = data[index_input3_1 - 1];
-            let data_input3_2 = data[index_input3_2 - 1];
-
-            let index_input4_1 = data.indexOf("input-Te1-0-01");
-            let index_input4_2 = data.indexOf("input-Te1-0-01", index_input4_1 + 1);
-
-            let data_input4_1 = data[index_input4_1 - 1];
-            let data_input4_2 = data[index_input4_2 - 1];
-
-            let index_input5_1 = data.indexOf("input-Te1-0-02");
-            let index_input5_2 = data.indexOf("input-Te1-0-02", index_input5_1 + 1);
-
-            let data_input5_1 = data[index_input5_1 - 1];
-            let data_input5_2 = data[index_input5_2 - 1];
-
-            let index_input6_1 = data.indexOf("input-Te1-0-03");
-            let index_input6_2 = data.indexOf("input-Te1-0-03", index_input6_1 + 1);
-
-            let data_input6_1 = data[index_input6_1 - 1];
-            let data_input6_2 = data[index_input6_2 - 1];
-
-            let traffic_input1 = (data_input1_2 - data_input1_1) / interval;
-            let traffic_input2 = (data_input2_2 - data_input2_1) / interval;
-            let traffic_input3 = (data_input3_2 - data_input3_1) / interval;
-            let traffic_input4 = (data_input4_2 - data_input4_1) / interval;
-            let traffic_input5 = (data_input5_2 - data_input5_1) / interval;
-            let traffic_input6 = (data_input6_2 - data_input6_1) / interval;
-
-            // let totalTraffic = traffic1;
-            // let unitTraffic = changeUnit(totalTraffic);
-            let unitTraffic_input1 = changeUnit(traffic_input1)
-            let unitTraffic_input2 = changeUnit(traffic_input2)
-            let unitTraffic_input3 = changeUnit(traffic_input3)
-            let unitTraffic_input4 = changeUnit(traffic_input4)
-            let unitTraffic_input5 = changeUnit(traffic_input5)
-            let unitTraffic_input6 = changeUnit(traffic_input6)
-
-            document.getElementById('kobra-e1-tps-value-in1').innerText = unitTraffic_input1.value.toFixed(1) + unitTraffic_input1.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-in2').innerText = unitTraffic_input2.value.toFixed(1) + unitTraffic_input2.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-in3').innerText = unitTraffic_input3.value.toFixed(1) + unitTraffic_input3.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-in4').innerText = unitTraffic_input4.value.toFixed(1) + unitTraffic_input4.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-in5').innerText = unitTraffic_input5.value.toFixed(1) + unitTraffic_input5.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-in6').innerText = unitTraffic_input6.value.toFixed(1) + unitTraffic_input6.unit + '/s';
-
-
-            let index_output1_1 = data.indexOf("output-Gi1-0-01");
-            let index_output1_2 = data.indexOf("output-Gi1-0-01", index_output1_1 + 1);
+            let index_output1_1 = data.indexOf("output-Gi1-25");
+            let index_output1_2 = data.indexOf("output-Gi1-25", index_output1_1 + 1);
 
             let data_output1_1 = data[index_output1_1 - 1];
             let data_output1_2 = data[index_output1_2 - 1];
 
-            let index_output2_1 = data.indexOf("output-Gi1-0-02");
-            let index_output2_2 = data.indexOf("output-Gi1-0-02", index_output2_1 + 1);
-
-            let data_output2_1 = data[index_output2_1 - 1];
-            let data_output2_2 = data[index_output2_2 - 1];
-
-            let index_output3_1 = data.indexOf("output-Gi1-0-03");
-            let index_output3_2 = data.indexOf("output-Gi1-0-03", index_output3_1 + 1);
-
-            let data_output3_1 = data[index_output3_1 - 1];
-            let data_output3_2 = data[index_output3_2 - 1];
-
-            let index_output4_1 = data.indexOf("output-Te1-0-01");
-            let index_output4_2 = data.indexOf("output-Te1-0-01", index_output4_1 + 1);
-
-            let data_output4_1 = data[index_output4_1 - 1];
-            let data_output4_2 = data[index_output4_2 - 1];
-
-            let index_output5_1 = data.indexOf("output-Te1-0-02");
-            let index_output5_2 = data.indexOf("output-Te1-0-02", index_output5_1 + 1);
-
-            let data_output5_1 = data[index_output5_1 - 1];
-            let data_output5_2 = data[index_output5_2 - 1];
-
-            let index_output6_1 = data.indexOf("output-Te1-0-03");
-            let index_output6_2 = data.indexOf("output-Te1-0-03", index_output6_1 + 1);
-
-            let data_output6_1 = data[index_output6_1 - 1];
-            let data_output6_2 = data[index_output6_2 - 1];
-
+   
             let traffic_output1 = (data_output1_2 - data_output1_1) / interval;
-            let traffic_output2 = (data_output2_2 - data_output2_1) / interval;
-            let traffic_output3 = (data_output3_2 - data_output3_1) / interval;
-            let traffic_output4 = (data_output4_2 - data_output4_1) / interval;
-            let traffic_output5 = (data_output5_2 - data_output5_1) / interval;
-            let traffic_output6 = (data_output6_2 - data_output6_1) / interval;
 
             // let totalTraffic = traffic1;
             // let unitTraffic = changeUnit(totalTraffic);
             let unitTraffic_output1 = changeUnit(traffic_output1)
-            let unitTraffic_output2 = changeUnit(traffic_output2)
-            let unitTraffic_output3 = changeUnit(traffic_output3)
-            let unitTraffic_output4 = changeUnit(traffic_output4)
-            let unitTraffic_output5 = changeUnit(traffic_output5)
-            let unitTraffic_output6 = changeUnit(traffic_output6)
 
-            document.getElementById('kobra-e1-tps-value-out1').innerText = unitTraffic_output1.value.toFixed(1) + unitTraffic_output1.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-out2').innerText = unitTraffic_output2.value.toFixed(1) + unitTraffic_output2.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-out3').innerText = unitTraffic_output3.value.toFixed(1) + unitTraffic_output3.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-out4').innerText = unitTraffic_output4.value.toFixed(1) + unitTraffic_output4.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-out5').innerText = unitTraffic_output5.value.toFixed(1) + unitTraffic_output5.unit + '/s';
-            document.getElementById('kobra-e1-tps-value-out6').innerText = unitTraffic_output6.value.toFixed(1) + unitTraffic_output6.unit + '/s';
+
+            document.getElementById('tof-area-value1').innerText = unitTraffic_output1.value.toFixed(1) + unitTraffic_output1.unit + '/s';
+
 
             // checkTraffic('ccsi1-line', 'ccsi1-value', totalTraffic, '10G');
 
@@ -630,9 +604,123 @@ function monitoringKoBRAe1TPSSwitch(interval) {
 
     xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.setRequestHeader("Authorization", "Token " + influxDBToken)
-    xhttp.send(JSON.stringify(queryData.kobraE1TPS));
+    xhttp.send(JSON.stringify(queryData.ndpsTOFMain));
 
     let date = new Date();
     let isoTime = date.toISOString().replace('Z', timezone);
-    queryData.kobraE1TPS.now = isoTime;
+    queryData.ndpsTOFMain.now = isoTime;
+}
+
+function monitoringNDPSPSRoomMainSwitch(interval) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let data = this.responseText.split(",");
+
+            let index_input1_1 = data.indexOf("input-eth1-01-13");
+            let index_input1_2 = data.indexOf("input-eth1-01-13", index_input1_1 + 1);
+
+            let data_input1_1 = data[index_input1_1 - 1];
+            let data_input1_2 = data[index_input1_2 - 1];
+
+            let traffic_input1 = (data_input1_2 - data_input1_1) / interval;
+
+            let unitTraffic_input1 = changeUnit(traffic_input1)
+
+            document.getElementById('ndps-storage-value').innerText = unitTraffic_input1.value.toFixed(1) + unitTraffic_input1.unit + '/s';
+
+            let index_output1_1 = data.indexOf("output-eth1-01-29");
+            let index_output1_2 = data.indexOf("output-eth1-01-29", index_output1_1 + 1);
+
+            let data_output1_1 = data[index_output1_1 - 1];
+            let data_output1_2 = data[index_output1_2 - 1];
+   
+            let traffic_output1 = (data_output1_2 - data_output1_1) / interval;
+
+            // let totalTraffic = traffic1;
+            // let unitTraffic = changeUnit(totalTraffic);
+            let unitTraffic_output1 = changeUnit(traffic_output1)
+
+
+            document.getElementById('ndps-ps-room-main-value').innerText = unitTraffic_output1.value.toFixed(1) + unitTraffic_output1.unit + '/s';
+
+
+            // checkTraffic('ccsi1-line', 'ccsi1-value', totalTraffic, '10G');
+
+        }
+    };
+
+    xhttp.open('POST', queryString, true);
+
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.setRequestHeader("Authorization", "Token " + influxDBToken)
+    xhttp.send(JSON.stringify(queryData.ndpsPSRoomMain));
+
+    let date = new Date();
+    let isoTime = date.toISOString().replace('Z', timezone);
+    queryData.ndpsPSRoomMain.now = isoTime;
+}
+
+
+function monitoringNDPSPSRoomSwitch(interval) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let data = this.responseText.split(",");
+
+            let index_input1_1 = data.indexOf("input-Gi1-02");
+            let index_input1_2 = data.indexOf("input-Gi1-02", index_input1_1 + 1);
+
+            let data_input1_1 = data[index_input1_1 - 1];
+            let data_input1_2 = data[index_input1_2 - 1];
+
+            let index_input2_1 = data.indexOf("input-Gi1-08");
+            let index_input2_2 = data.indexOf("input-Gi1-08", index_input2_1 + 1);
+
+            let data_input2_1 = data[index_input2_1 - 1];
+            let data_input2_2 = data[index_input2_2 - 1];
+
+            let traffic_input1 = (data_input1_2 - data_input1_1) / interval;
+            let traffic_input2 = (data_input2_2 - data_input2_1) / interval;
+
+            // let totalTraffic = traffic1;
+            // let unitTraffic = changeUnit(totalTraffic);
+            let unitTraffic_input1 = changeUnit(traffic_input1)
+            let unitTraffic_input2 = changeUnit(traffic_input2)
+
+            document.getElementById('ndps-archive-value').innerText = unitTraffic_input1.value.toFixed(1) + unitTraffic_input1.unit + '/s';
+            document.getElementById('ndps-virtual-value').innerText = unitTraffic_input2.value.toFixed(1) + unitTraffic_input2.unit + '/s';
+
+            let index_output1_1 = data.indexOf("output-Te1-49");
+            let index_output1_2 = data.indexOf("output-Te1-49", index_output1_1 + 1);
+
+            let data_output1_1 = data[index_output1_1 - 1];
+            let data_output1_2 = data[index_output1_2 - 1];
+   
+            let traffic_output1 = (data_output1_2 - data_output1_1) / interval;
+
+            // let totalTraffic = traffic1;
+            // let unitTraffic = changeUnit(totalTraffic);
+            let unitTraffic_output1 = changeUnit(traffic_output1)
+
+
+            document.getElementById('ndps-ps-room-value').innerText = unitTraffic_output1.value.toFixed(1) + unitTraffic_output1.unit + '/s';
+
+
+            // checkTraffic('ccsi1-line', 'ccsi1-value', totalTraffic, '10G');
+
+        }
+    };
+
+    xhttp.open('POST', queryString, true);
+
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.setRequestHeader("Authorization", "Token " + influxDBToken)
+    xhttp.send(JSON.stringify(queryData.ndpsPSRoom));
+
+    let date = new Date();
+    let isoTime = date.toISOString().replace('Z', timezone);
+    queryData.ndpsPSRoom.now = isoTime;
 }
